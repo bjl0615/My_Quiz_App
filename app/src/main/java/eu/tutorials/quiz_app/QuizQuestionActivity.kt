ac62fbc7f92261d6dbc45e674218675864c9b6bf
bjl0615 <bjl0615@gmail.com>
2023-02-09 19:38:05 +0900

    Question_UI_Model Skill

--- app/src/main/java/eu/tutorials/quiz_app/QuizQuestionActivity.kt
+++ app/src/main/java/eu/tutorials/quiz_app/QuizQuestionActivity.kt
@@ -1,14 +1,23 @@
 package eu.tutorials.quiz_app
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import java.util.ArrayList
 
-class QuizQuestionActivity : AppCompatActivity() {
+class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
 
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList:ArrayList<Question>? = null
+    private var meSelectedOptionPostion : Int = 1
 
     //Create global variables for the views in the layout
     private var progressBar: ProgressBar?=null
@@ -19,6 +28,8 @@ class QuizQuestionActivity : AppCompatActivity() {
     private var tvOptionTwo:TextView? = null
     private var tvOptionThree:TextView? = null
     private var tvOptionFour:TextView? = null
+    private var btnSubmit : Button ? = null
+
     /**
      * This function is auto created by Android when the Activity Class is created.
      */
@@ -27,20 +38,6 @@ class QuizQuestionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // This is used to align the xml view to this class
         setContentView(R.layout.activity_quiz_question)
-
-        val questionsList = Constants.getQuestions()
-        Log.e("Questions Size", "${questionsList.size}")
-        for (i in questionsList) {
-            Log.e("Questions", i.question)
-        }
-        // END
-
-        // TODO (Step 3: Setting the question in the UI from the list.)
-        // START
-        val currentPosition = 1 // Default and the first question position
-        val question: Question? =
-            questionsList[currentPosition - 1] // Getting the question from the list with the help of current position.
-
         progressBar=findViewById(R.id.progressBar)
         tvProgress = findViewById(R.id.tv_progress)
         tvQuestion = findViewById(R.id.tv_question)
@@ -49,17 +46,108 @@ class QuizQuestionActivity : AppCompatActivity() {
         tvOptionTwo = findViewById(R.id.tv_option_two)
         tvOptionThree = findViewById(R.id.tv_option_three)
         tvOptionFour = findViewById(R.id.tv_option_four)
-        progressBar?.progress =
-            currentPosition // Setting the current progress in the progressbar using the position of question
-        tvProgress?.text =
-            "$currentPosition" + "/" + progressBar?.getMax() // Setting up the progress text
+        btnSubmit = findViewById(R.id.btn_submit)
 
-        // Now set the current question and the options in the UI
-        tvQuestion?.text = question!!.question
+        tvOptionOne?.setOnClickListener(this)
+        tvOptionTwo?.setOnClickListener(this)
+        tvOptionThree?.setOnClickListener(this)
+        tvOptionFour?.setOnClickListener(this)
+        btnSubmit?.setOnClickListener(this)
+
+        mQuestionsList = Constants.getQuestions()
+
+        setQuestion()
+    }
+
+    private fun setQuestion() {
+
+        val question: Question = mQuestionsList!![mCurrentPosition - 1] // Getting the question from the list with the help of current position.
         ivImage?.setImageResource(question.image)
+        progressBar?.progress = mCurrentPosition // Setting the current progress in the progressbar using the position of question
+        tvProgress?.text = "$mCurrentPosition/${progressBar?.max}"
+        tvQuestion?.text = question.question
         tvOptionOne?.text = question.optionOne
         tvOptionTwo?.text = question.optionTwo
         tvOptionThree?.text = question.optionThree
         tvOptionFour?.text = question.optionFour
+
+        if(mCurrentPosition == mQuestionsList!!.size) {
+            btnSubmit?.text = "FINISH"
+        }else {
+            btnSubmit?.text = "SUBMIT"
+        }
+
+
+
+    }
+
+    private fun defaultOptionView() {
+        val options = ArrayList<TextView>()
+        tvOptionOne?.let {
+            options.add(0, it)
+        }
+        tvOptionTwo?.let {
+            options.add(1, it)
+        }
+        tvOptionThree?.let {
+            options.add(2, it)
+        }
+        tvOptionFour?.let {
+            options.add(3, it)
+        }
+
+        for(option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    private fun selectedOptionView(tv:TextView , selectedOptionNum : Int) {
+        defaultOptionView()
+
+        meSelectedOptionPostion = selectedOptionNum
+
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.tv_option_one -> {
+                tvOptionOne?.let {
+                    selectedOptionView(it, 1)
+                }
+            }
+
+            R.id.tv_option_two -> {
+                tvOptionTwo?.let {
+                    selectedOptionView(it, 2)
+                }
+            }
+
+            R.id.tv_option_three -> {
+                tvOptionThree?.let {
+                    selectedOptionView(it, 3)
+                }
+            }
+
+            R.id.tv_option_four -> {
+                tvOptionFour?.let {
+                    selectedOptionView(it, 4)
+                }
+            }
+
+            R.id.btn_submit -> {
+
+            }
+        }
     }
 }
